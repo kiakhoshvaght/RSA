7c67648ca1491e44a57d2638aa48a50d66c349ef
kia khoshvaght <k.khoshvaght>
2019-06-10 13:41:31 +0430

    encrypted text is shown to user with base 64 conversion.

--- app/src/main/java/kiakhoshvaght/rsa/MainActivity.java
+++ app/src/main/java/kiakhoshvaght/rsa/MainActivity.java
@@ -1,12 +1,18 @@
 package kiakhoshvaght.rsa;
 
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -17,7 +23,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @BindView(R.id.input_et)
     protected EditText inputEt;
     @BindView(R.id.result_tv)
-    protected TextView resultTv;
+    protected EditText resultTv;
 
     private static final String TAG = MainActivity.class.getName();
     private RSA rsa = new RSA();
@@ -29,6 +35,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         ButterKnife.bind(this);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @OnClick({
             R.id.encrypt_btn,
             R.id.decrypt_btn
@@ -38,13 +45,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Log.i(TAG,"FUNCTION : onClick");
         switch (view.getId()){
             case R.id.encrypt_btn:
-                byte[] ecrypted = rsa.encrypt(inputEt.getText().toString().getBytes());
-                Log.i(TAG,"FUNCTION : onClick => Encrypt: " + bytesToString(ecrypted));
-                resultTv.setText(bytesToString(ecrypted));
+                String text = inputEt.getText().toString();
+                Log.i(TAG,"FUNCTION : onClick => Encrypt => text: " + text);
+                byte[] encrypted = rsa.encrypt(text.getBytes());
+                Log.i(TAG, "FUNCTION : onClick => Encrypt => text in bytes: " + bytesToString(text.getBytes()));
+                Log.i(TAG,"FUNCTION : onClick => Encrypt: " + bytesToString(encrypted));
+                resultTv.setText(Base64.encodeToString(encrypted, Base64.DEFAULT));
+                inputEt.setText(new String(rsa.decrypt(encrypted)));
                 break;
             case  R.id.decrypt_btn:
                 Log.i(TAG,"FUNCTION : onClick => Decrypt");
-                resultTv.setText(rsa.decrypt(inputEt.getText().toString().getBytes()).toString());
+                Charset charset = StandardCharsets.US_ASCII;
+                byte[] decrypted = rsa.decrypt(Base64.decode(inputEt.getText().toString(),Base64.DEFAULT));
+                Log.i(TAG,"FUNCTION : onClick => Decrypt => Decrypted in bytes: " + decrypted.toString());
+                resultTv.setText(new String(decrypted));
                 break;
         }
     }
--- app/src/main/java/kiakhoshvaght/rsa/RSA.java
+++ app/src/main/java/kiakhoshvaght/rsa/RSA.java
@@ -1,27 +1,20 @@
 package kiakhoshvaght.rsa;
 
-import java.io.DataInputStream;
-import java.io.IOException;
 import java.math.BigInteger;
 import java.util.Random;
 
 public class RSA {
-    private BigInteger p;
-    private BigInteger q;
-    private BigInteger N;
-    private BigInteger phi;
-    private BigInteger e;
-    private BigInteger d;
-    private int bitlength = 1024;
+    private BigInteger p, q, N, e, phi, d;
+    private int primeLength = 1024;
     private Random r;
 
     public RSA() {
         r = new Random();
-        p = BigInteger.probablePrime(bitlength, r);
-        q = BigInteger.probablePrime(bitlength, r);
+        p = BigInteger.probablePrime(primeLength, r);
+        q = BigInteger.probablePrime(primeLength, r);
         N = p.multiply(q);
         phi = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
-        e = BigInteger.probablePrime(bitlength / 2, r);
+        e = BigInteger.probablePrime(primeLength / 2, r);
         while (phi.gcd(e).compareTo(BigInteger.ONE) > 0 && e.compareTo(phi) < 0) {
             e.add(BigInteger.ONE);
         }
@@ -32,32 +25,6 @@ public class RSA {
         this.e = e;
         this.d = d;
         this.N = N;
-    }
-
-    @SuppressWarnings("deprecation")
-    public static void main(String[] args) throws IOException {
-        RSA rsa = new RSA();
-        DataInputStream in = new DataInputStream(System.in);
-        String teststring;
-        System.out.println("Enter the plain text:");
-        teststring = in.readLine();
-        System.out.println("Encrypting String: " + teststring);
-        System.out.println("String in Bytes: "
-                + bytesToString(teststring.getBytes()));
-
-        byte[] encrypted = rsa.encrypt(teststring.getBytes());
-
-        byte[] decrypted = rsa.decrypt(encrypted);
-        System.out.println("Decrypting Bytes: " + bytesToString(decrypted));
-        System.out.println("Decrypted String: " + new String(decrypted));
-    }
-
-    private static String bytesToString(byte[] encrypted) {
-        String test = "";
-        for (byte b : encrypted) {
-            test += Byte.toString(b);
-        }
-        return test;
     }
 
     public byte[] encrypt(byte[] message) {
